2da9e6c3072ea92f8e7f4cc30a1b2cec08fe34d7
Brandhoej <akbr@cs.aau.dk>
2025-03-14 14:05:15 +0100

    fixed tests to account for flips

--- fill_test.py
+++ fill_test.py
@@ -167,7 +167,7 @@ class TestRISCVProgramParse(unittest.TestCase):
 
     def test_parse_verify_pin_0(self):
         path = './FISSC/u0000 INLINED RISC-V (32-bits) gcc 14.2.0/VerifyPIN_0.asm'
-        program = RISCVProgram.parse(path, 0)
+        program = RISCVProgram.parse(path, 0, 0)
     
         self.assertEqual(len(program.symbols), 4)
 
@@ -218,7 +218,7 @@ class TestRISCVProgramParse(unittest.TestCase):
 
     def test_generated_verify_pin_2(self):
         path = './FISSC/u0000 INLINED RISC-V (32-bits) gcc 14.2.0/VerifyPIN_2_HB+FTL.asm'
-        program = RISCVProgram.parse(path, 0)
+        program = RISCVProgram.parse(path, 0, 0)
 
         program_length = 'const int32_t PROGRAM_LENGTH = 83;'
         self.assertEqual(program.generated_program_length(), program_length)
@@ -433,7 +433,7 @@ class TestRISCVProgramParse(unittest.TestCase):
 
     def test_parse_test_example_verify_pin_0(self):
         path = './test_examples/VerifyPIN_0.asm'
-        program = RISCVProgram.parse(path, 0)
+        program = RISCVProgram.parse(path, 0, 0)
         self.assertEqual(len(program.assertions), 1)
 
     def test_parse_verify_pins(self):
@@ -449,7 +449,7 @@ class TestRISCVProgramParse(unittest.TestCase):
         ]
 
         for (path, symbols, length) in files:
-            program = RISCVProgram.parse(path, 0)
+            program = RISCVProgram.parse(path, 0, 0)
             self.assertEqual(len(program.symbols), symbols)
             self.assertEqual(program.length, length)
     
